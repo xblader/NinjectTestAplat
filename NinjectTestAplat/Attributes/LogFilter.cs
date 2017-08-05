e768c53dc9d16d437fc76f86dfb07c4879f99e96
xblader <xblader@gmail.com>
2017-08-05 08:52:46 -0300

    ok

---
+++ NinjectTestAplat/Attributes/LogFilter.cs
@@ -0,0 +1,46 @@
+﻿using log4net;
+using Ninject.Web.WebApi.Filter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+
+namespace NinjectTestAplat.Attributes
+{
+    public class LogFilter : AbstractActionFilter
+    {
+        private readonly ILog log;
+        private readonly string prefix;
+
+        public LogFilter(ILog log)
+        {
+            this.log = log;
+        }
+
+        public override bool AllowMultiple
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            this.log.DebugFormat(
+                "Executing action {0}.{1}",
+                actionContext.ActionDescriptor.ControllerDescriptor.ControllerName,
+                actionContext.ActionDescriptor.ActionName);
+        }
+
+        public override void OnActionExecuted(System.Web.Http.Filters.HttpActionExecutedContext actionExecutedContext)
+        {
+            this.log.DebugFormat(
+                "Executed action {0}.{1}",
+                actionExecutedContext.ActionContext.ActionDescriptor.ControllerDescriptor.ControllerName,
+                actionExecutedContext.ActionContext.ActionDescriptor.ActionName);
+        }
+    }
+}
